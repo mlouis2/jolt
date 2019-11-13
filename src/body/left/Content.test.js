1d4b21685e68f47c4ff82659221edc1a4fb9bca7
Maddie Louis <mlouis2@lion.lmu.edu>
2019-11-13 12:22:17 -0800

    removed api test because realized i don't need it

--- src/body/left/Content.test.js
+++ src/body/left/Content.test.js
@@ -3,17 +3,13 @@ import ReactDOM from "react-dom";
 import Content from "./Content";
 import { render, waitForElement, fireEvent } from "@testing-library/react";
 import ReactTestUtils, { act } from "react-dom/test-utils";
-import mockApi from "../api-placeholder";
-import realApi from "../api-real";
+import api from "../api";
 
-const useMock = true;
 const testNumPokemon = 20;
 
 it("renders without crashing", async () => {
   await act(async () => {
-    const content = render(
-      <Content api={useMock ? mockApi : realApi} numPokemon={testNumPokemon} />
-    );
+    const content = render(<Content api={api} numPokemon={testNumPokemon} />);
     expect(content).toMatchSnapshot();
   });
 });
@@ -21,7 +17,7 @@ it("renders without crashing", async () => {
 it("lets user click on the next slide", async () => {
   await act(async () => {
     const { getByTestId } = render(
-      <Content api={useMock ? mockApi : realApi} numPokemon={testNumPokemon} />
+      <Content api={api} numPokemon={testNumPokemon} />
     );
     const secondSlide = await waitForElement(() => getByTestId("1"));
     expect(secondSlide).not.toBeNull();
@@ -33,10 +29,7 @@ it("updates info box when user clicks on next slide", async () => {
   let div;
   await act(async () => {
     div = document.createElement("div");
-    ReactDOM.render(
-      <Content api={useMock ? mockApi : realApi} numPokemon={testNumPokemon} />,
-      div
-    );
+    ReactDOM.render(<Content api={api} numPokemon={testNumPokemon} />, div);
   });
   const secondSlide = div.querySelector("#Ivysaur");
 
